73b3ff1ca8b25a9624b4f1e7f7b516559ec9c9c4
kimgumshek <pasuwatw@gmail.com>
2020-04-24 13:33:29 +0700

    DEMO Spring Boot web with Thymeleaf

---
+++ src/main/java/demo/controller/DemoController.java
@@ -0,0 +1,29 @@
+package demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
+
+@Controller
+public class DemoController {
+
+    @RequestMapping("/")
+    public @ResponseBody String hi(){
+        return "Hello Spring Boot";
+    }
+
+    @RequestMapping("/hi/{name}")
+    public @ResponseBody String sayHiWithName(@PathVariable String name){
+        return "Hello "+name;
+    }
+
+    @RequestMapping("/hi/thymeleaf/{name}")
+    public String hiThymeLeaf(Map model, @PathVariable String name){
+        model.put("name",name);
+        return "hello";
+    }
+
+}
